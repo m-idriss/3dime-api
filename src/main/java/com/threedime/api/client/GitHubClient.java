69db22a29e9f13968019b3b97d60e065e57680bd
Idriss <44480885+m-idriss@users.noreply.github.com>
2026-02-15 13:24:14 +0100

    Add health checks and enhance logging configuration

--- src/main/java/com/threedime/api/client/GitHubClient.java
+++ src/main/java/com/threedime/api/client/GitHubClient.java
@@ -5,14 +5,18 @@ import jakarta.ws.rs.Path;
 import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
+import org.eclipse.microprofile.faulttolerance.Retry;
+import org.eclipse.microprofile.faulttolerance.Timeout;
 import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
 
 @Path("/users")
 @RegisterRestClient(configKey = "com.threedime.api.client.GitHubClient")
 public interface GitHubClient {
-    
+
     @GET
     @Path("/{username}")
     @Produces(MediaType.APPLICATION_JSON)
+    @Retry(maxRetries = 3, delay = 200)
+    @Timeout(2000)
     GitHubUser getUser(@PathParam("username") String username);
 }
